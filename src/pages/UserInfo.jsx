8ae87617f2d9d925f08abd16061786011b1bcb3a
Dafna Bashan <dafnaba1@gmail.com>
2023-01-31 11:43:56 +0200

    user info ui

--- src/pages/UserInfo.jsx
+++ src/pages/UserInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { NavBar } from '../cmps/NavBar'
+import userImg from '../assets/img/user-img.png'
 
 export const UserInfo = () => {
 
@@ -8,7 +9,7 @@ export const UserInfo = () => {
         name: 'Xanthe Neal',
         bio: 'I am a software developer and a big fan of devchallenges...',
         phone: '908249274292',
-        imgUrl: '',
+        imgUrl: userImg,
         email: 'xanthe.neal@gmail.com',
         password: '************'
     }
@@ -18,40 +19,40 @@ export const UserInfo = () => {
     return (
         <React.Fragment>
             <NavBar />
-            <div>
-                <div>
-                    <div>Personal info</div>
-                    <div>Basic info, like your name and photo</div>
+            <div className="user-info">
+                <div className="center">
+                    <div className="title">Personal info</div>
+                    <div className="subtitle">Basic info, like your name and photo</div>
                 </div>
-                <div>
-                    <div>
-                        <div>
-                            <div>Profile</div>
-                            <div>Some info may be visible to other people</div>
+                <div className="frame">
+                    <div className="profile-container flex align-center">
+                        <div className="full">
+                            <div className="profile" style={{ paddingLeft: 0 }}>Profile</div>
+                            <div className="profile-sub">Some info may be visible to other people</div>
                         </div>
-                        <Link to="/user/1/edit">Edit</Link>
+                        <Link to="/user/1/edit" className="edit">Edit</Link>
                     </div>
-                    <div>
+                    <div className="field img-con">
                         <div>PHOTO</div>
                         <img src={imgUrl} alt="userImg" />
                     </div>
-                    <div>
+                    <div className="field">
                         <div>NAME</div>
                         <div>{name}</div>
                     </div>
-                    <div>
+                    <div className="field">
                         <div>BIO</div>
                         <div>{bio}</div>
                     </div>
-                    <div>
+                    <div className="field">
                         <div>PHONE</div>
                         <div>{phone}</div>
                     </div>
-                    <div>
+                    <div className="field">
                         <div>EMAIL</div>
                         <div>{email}</div>
                     </div>
-                    <div>
+                    <div className="field">
                         <div>PASSWORD</div>
                         <div>{password}</div>
                     </div>
