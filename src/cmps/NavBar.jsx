c745fef5c8acd9f06ac7acc9225554d26b19828f
Dafna Bashan <dafnaba1@gmail.com>
2023-03-16 15:40:00 +0200

    toggle visibility user manu

--- src/cmps/NavBar.jsx
+++ src/cmps/NavBar.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import ClickAwayListener from '@mui/base/ClickAwayListener';
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import userImg from '../assets/img/user-img.png'
@@ -10,13 +11,17 @@ export const NavBar = () => {
   const loggedInUser = useSelector(state => state.userModule.loggedInUser)
   const navigate = useNavigate()
 
-  const closeManu = () => {
-    setIsManuOpen(false)
-  }
+  // const closeManu = () => {
+  //   setIsManuOpen(false)
+  // }
 
+  const toggleManu = () => {
+    if (isManuOpen) setIsManuOpen(false)
+    else setIsManuOpen(true)
+  }
   useEffect(() => {
     if (!loggedInUser) navigate('/login')
-}, [loggedInUser, navigate])
+  }, [loggedInUser, navigate])
 
   const testUser = {
     name: 'Xanthe Neal',
@@ -30,8 +35,13 @@ export const NavBar = () => {
   return (
     <div className="nav-bar flex space-between">
       <div>Auth App</div>
-      <div onClick={() => setIsManuOpen(true)}><img src={userImg} alt="" />{testUser.name}</div>
-      {isManuOpen && <UserManu closeFunc={closeManu}/>}
+      <div onClick={toggleManu}><img src={userImg} alt="" />{testUser.name}</div>
+
+      {/* <ClickAwayListener onClickAway={()=> setIsManuOpen(false)}>
+        <div style={{position: 'absolute'}}> */}
+          {isManuOpen && <UserManu closeFunc={()=> setIsManuOpen(false)} />}
+        {/* </div> */}
+      {/* </ClickAwayListener> */}
     </div>
   )
 }
