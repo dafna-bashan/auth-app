7c28121881be4a7949691fe1cdc519110d8a9963
Dafna Bashan <dafnaba1@gmail.com>
2023-03-30 10:02:10 +0300

    bcrypt

--- backend/api/auth/auth.service.js
+++ backend/api/auth/auth.service.js
@@ -1,4 +1,4 @@
-// const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt')
 const userService = require('../user/user.service');
 const logger = require('../../services/logger.service');
 
@@ -6,34 +6,33 @@ async function login(email, password) {
     logger.debug(`auth.service - login with email: ${email}`);
 
     const user = await userService.getByEmail(email);
-    if (!user || user.password !== password) {
+    if (!user) {
         console.log('line 10 Auth service - wrong cred!!!!')
         return Promise.reject('Invalid email or password');
     }
+    // TODO: un-comment for real login
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) return Promise.reject('Invalid email or password')
     delete user.password;
     return user;
-    // TODO: un-comment for real login
-    // const match = await bcrypt.compare(password, user.password)
-    // if (!match) return Promise.reject('Invalid email or password')
 }
 
 async function signup(userCred) {
     console.log('auth service', userCred)
-    const {email, firstName, lastName, password} = userCred
+    const { email, firstName, lastName, password } = userCred
     logger.debug(
         `auth.service - signup with email: ${email}, fullname: ${firstName} ${lastName}`
     );
-    if (!email || !password || !firstName || !lastName){
+    if (!email || !password || !firstName || !lastName) {
         return Promise.reject('fullname, email and password are required!');
     }
     const users = await userService.query()
     const isUserExist = users.find(user => user.email === email)
     if (isUserExist) return Promise.reject('email already exists')
 
-
-    // const hash = await bcrypt.hash(password, saltRounds)
-    // return userService.add({ ...user, password: hash })
-    return userService.add(userCred);
+    const saltRounds = 10
+    const hash = await bcrypt.hash(password, saltRounds)
+    return userService.add({ ...userCred, password: hash })
 }
 
 module.exports = {
