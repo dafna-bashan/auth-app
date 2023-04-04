4952f501ce628b743ae792d79637846efe792ee5
Dafna Bashan <dafnaba1@gmail.com>
2023-04-04 14:31:45 +0300

    trying to fix logger issue prod 3

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  dbURL: 'mongodb+srv://dafnaba1:TBg74UzGXmQyhbbK@chat-app-db.1843avm.mongodb.net/test',
+  dbURL: 'mongodb+srv://dafnaba1:TBg74UzGXmQyhbbK@chat-app-db.1843avm.mongodb.net/test?retryWrites=true&w=majority',
   dbName: 'CHAT_APP_DB'
 }
